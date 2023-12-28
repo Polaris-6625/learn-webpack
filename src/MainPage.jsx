47160ab147d0a65a2bf2364dee205a97e557a398
Yuyang Liu <yuyang.liu@rightcapital.com>
2023-12-28 17:39:17 +0800

    feat: add history router mode

---
+++ src/MainPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import './global.css';
+import IndexPage from './pages/index';
+import Information from './pages/information';
+
+const MainPage = () => {
+  const [currentPage, setCurrentPage] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setCurrentPage(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, []);
+
+  function navigateTo(targetPage) {
+    console.log(`Navigating to /${targetPage}`);
+    window.history.pushState({}, '', `/${targetPage}`);
+    setCurrentPage(`/${targetPage}`);
+  }
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case '/':
+      case '/index':
+        return <IndexPage />;
+      case '/information':
+        return <Information />;
+      default:
+        return <div>怎么寄了...</div>;
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <button onClick={() => navigateTo('index')}>index</button>
+        <button onClick={() => navigateTo('information')}>information</button>
+      </div>
+      {renderPage()}
+    </>
+  );
+};
+
+export default MainPage;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import MainPage from "./MainPage";
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+ReactDOM.render(<MainPage/>,document.getElementById("root"));
